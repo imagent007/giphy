f2c5388e7f5823bca2c9d2f4d3c2881af362f8a4
Vaughn <vgree007@gmail.com>
2019-03-20 21:55:43 +0000

    pushone

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  $.ajax({
+  url: "https://slack-redir.net/link?url=https%3A%2F%2Fapi.giphy.com%2Fv1%2Fgifs%2Fsearch%3Fq%3Dpuppy%26rating%3Dpg%26api_key%3DcVRNyx7QJZX0F6nTWQ0XbssO1yb4KtSM",
+method: "GET",
+  success: function(response) {
+    // function runs if successful
+    console.log(response.data[0])
+  },
+});
+
   
   
 });
